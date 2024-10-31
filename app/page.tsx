6082b01a8c847356033aaf929cd003a3d066fb98
vaqsi1990 <97800985+vaqsi1990@users.noreply.github.com>
2024-10-31 19:24:54 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,25 @@
-import React from 'react'
-
-import CategoriesList from '@/components/home/CategoriesList'
-import PropertiesContainer from '@/components/home/PropertiesContainer'
-import { Suspense } from 'react'
-import LoadingCards from '@/components/card/LoadingCards'
-function HomePage({searchParams}:{searchParams:{category?:string, search?:string}}) {
-  console.log('es', process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+import LoadingCards from '@/components/card/LoadingCards';
+import CategoriesList from '@/components/home/CategoriesList';
+import PropertiesContainer from '@/components/home/PropertiesContainer';
+import { Suspense } from 'react';
+function HomePage({
+  searchParams,
+}: {
+  searchParams: { category?: string; search?: string };
+}) {
   return (
-   <section>
-    <CategoriesList category={searchParams.category} search={searchParams.search} />
-    <Suspense fallback={ < LoadingCards />} >
-    <PropertiesContainer />
-    </Suspense>
-   </section>
-  )
+    <section>
+      <CategoriesList
+        category={searchParams.category}
+        search={searchParams.search}
+      />
+      <Suspense fallback={<LoadingCards />}>
+        <PropertiesContainer
+          category={searchParams.category}
+          search={searchParams.search}
+        />
+      </Suspense>
+    </section>
+  );
 }
-
-export default HomePage
+export default HomePage;
